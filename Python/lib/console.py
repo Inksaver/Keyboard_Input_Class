b8df08d91edf01332ae94ad2ee475d96f606666e
Inksaver <Inksaver@users.noreply.github.com>
2022-02-21 19:50:11 +0000

    Update console.py

--- Python/lib/console.py
+++ Python/lib/console.py
@@ -3,8 +3,7 @@ This class is used to get console information.
 On Windows ANSI codes can be 'reluctant' to start, so clearing the console is usually the best start
 USE: import console" (or folder.console if stored elsewhere)
 Call any console function eg console.clear() or console.initialise():
-console.initialise() -> runs console.initialise() and sets isInitialised = true
-Console.Clear() -> runs Console.Initialise() and sets isInitialised = true
+console.clear() -> runs console.initialise() sets is_initialised = True, then clears the screen
 populates properties:
 os_name
 is_console
@@ -22,28 +21,25 @@ utf8 box characters stored here for easy copy/paste:
 '''
 import os
 
-window_width = 0
-window_height = 0
-is_console = False
-os_name = ""
-is_initialised = False
-CLEARLINE = '\x1b[2K'  	# clear the line
+window_width:int    = 0
+window_height:int   = 0
+is_console:bool     = False
+os_name:str         = ""
+is_initialised:bool = False
+CLEARLINE:str       = '\x1b[2K'  	# ansi code to clear the current line
 
-def add_lines(num_lines, current_lines = None): #default 30 lines if not given
-	''' overloaded Python function: supply None as 2nd parameter'''
+def add_lines(num_lines:int, current_lines:int = 0) -> int: #default 30 lines if not given
+	''' adds blank lines to non-console IDE to simulate screen clearing '''
 	# use 1: add_lines(5) adds 5 additional blank lines
-	# use 2: add_lines(5, 19) adds sufficient lines to fill console to last 5 lines (as 19 are already used)
-	blank = "".rjust(window_width); #string of spaces across entire width of Console
-	if current_lines is None:
-		if num_lines > 0:
-			for i in range(num_lines):
-				print(blank)
-	else:
+	# use 2: add_lines(5, 19) adds sufficient lines to fill 25 line console to last 5 lines (as 19 are already used)
+	blank = "".rjust(window_width); # string of spaces across entire width of Console
+	if current_lines != 0:
 		leave_lines = num_lines
 		num_lines = window_height - current_lines - leave_lines;
-		if num_lines > 0:
-			for i in range(num_lines):
-				print(blank)
+		
+	if num_lines > 0:
+		for i in range(num_lines):
+			print(blank)
 
 	return num_lines;	
 
@@ -59,11 +55,12 @@ def clear() -> None:
 		add_lines(window_height)
 		return -1		# if running in ide row = -1
 		
-def clear_line(line_no):
+def clear_line(line_no:int) -> None:
+	''' Clear one line in console '''
 	set_cursor_pos(1, line_no)
 	print(CLEARLINE, end = '')
 	
-def initialise():
+def initialise() -> None:
 	''' runs on load to get console/terminal info '''
 	global window_width, window_height, is_console, is_initialised
 	if not is_initialised:
@@ -77,9 +74,12 @@ def initialise():
 			is_console = False
 		is_initialised = True
 		
-def resize(window_width, window_height, clear = True):
+def resize(width:int, height:int, clear:bool = True) -> None:
 	''' resize the console / terminal '''
+	global window_width, window_height
 	if is_console:
+		window_width = width      # set global window_width
+		window_height = height    # set global window_height
 		if os.name == 'nt':
 			os.system(f'mode {window_width},{window_height}')
 		else:    
