9e0a780db5fd2a82cf21cdb0660a37b33215970a
Inksaver <Inksaver@users.noreply.github.com>
2022-02-26 08:33:26 +0000

    Update kboard.py

--- Python/lib/kboard.py
+++ Python/lib/kboard.py
@@ -1,17 +1,16 @@
-import time
 '''
-kboard static class returns string, integer, float, boolean and menu choices
+kboard static class returns string, integer, float, boolean and menu choices.
 Use:
 import kboard as kb #shortens the class name to kb
 
 Adding a current console row number at the end of each method overwrites errors on the same line
 eg. if the console has already got 5 lines of output:
-user_name = kb.get_string("Type your name", True, 5) will display messages over the prompt on line 6
+user_name = kb.get_string("What is your name?", True, 5) will display messages over the prompt on line 6
 for 2 seconds, then re-display the prompt.
 
 The delay can be over-ridden with kb.delay = 3.5 for a longer delay
 
-user_name = kb.get_string("Type your name", True) # returns user input in Title Case
+user_name = kb.get_string("What is your name?", True) # returns user input in Title Case
 
 user_age = kb.get_integer("How old are you", 5, 120) # gets an integer between 5 and 120 from the user
 
@@ -26,15 +25,17 @@ print(f"User thinks this utility is: {menu_list[user_choice]}")
 kb.get_string("Press Enter to Quit", False, 0, 20) # Used instead of input("Press Enter to Quit")
 at the end of a file to prevent the console closing (does not apply when run from IDE)
 '''
+import time
+
 blank = "".ljust(79," ")
 delay = 2
 
 def process_input(prompt:str, min_value:int, max_value:int, data_type:str, row) -> str:
-	''' This function is not called directly from other files. Python does not have a 'Private' keyword'''
+	''' private method: validate input, raise error messages until input is valid '''
 	valid_input:bool = False
 	while valid_input is False:
-		clear_input_field(row)
-		user_input:str = input(prompt + "_").strip()		# remove leading and trailing spaces
+		clear_input_field(row)			
+		user_input:str = input(f"{prompt.strip()}_")		# remove leading and trailing spaces
 		output = user_input									# used later if user_input converted to int/float
 		if data_type == "string":							# string requested
 			if len(user_input) == 0 and min_value > 0:		# enter pressed when min characters > 0
@@ -106,23 +107,47 @@ def get_boolean(prompt:str, row:int = -1) -> bool:
 		row += 1	
 	return process_input(prompt, 1, 3, "bool", row)
 
-def menu(title:str, menu_list:list, row = -1) -> int:
-	''' displays a menu using the text in 'title', and a list of menu items (string) '''
+def get_max_len(title, menu_list = None, window_width = 80):
+	max_len = len(title)					# calculate width of menu display
+	if menu_list != None:
+		for line in menu_list:				# get length of each menu option (inc #) ))
+			if len(line) + 9 > max_len:		# include "     1) " for each line
+				max_len = len(line) + 9		# change max_len
+
+	max_len += 28							# add packing spaces
+	if max_len > window_width - 2:			# limit width to console size or 78
+		max_len = window_width - 2
+	if max_len % 2 == 1:					# check even number length
+		max_len += 1
+		
+	return max_len
+
+def menu(title:str, menu_list:list, row = -1, window_width = 80) -> int:
+	''' public method. Displays a menu using the text in 'title', and a list of menu items (string) '''
+	if len(title) % 2 == 1:					# is title length an odd number?
+		title += " "						# add extra space to even up the length
+		
 	rows = -1
 	if row >= 0:
-		rows = row + len(menu_list) + 2	
-	print(title)
-	for i in range(len(menu_list)):
-		if i < 9:
-			print(f"     {i+1}) {menu_list[i]}")
+		rows = row + len(menu_list) + 4
+		
+	max_len = get_max_len(title, menu_list)
+	
+	filler = "".ljust((max_len - len(title)) // 2) 	# calculate "    " filler spacing
+	print(f"╔{''.ljust(max_len,'═')}╗")				# draw top bar
+	print(f"║{filler}{title}{filler}║")				# draw title
+	print(f"╠{''.ljust(max_len,'═')}╣")				# draw middle bar
+	for i in range(len(menu_list)):					# draw options
+		if i < 10:
+			print(f"║     {i+1}) {menu_list[i].ljust(max_len - 8,' ')}║")
 		else:
-			print(f"    {i+1}) {menu_list[i]}") 
-	print("".ljust(80,"═"))
+			print(f"║    {i+1}) {menu_list[i].ljust(max_len - 7,' ')}║") 
+	print(f"╚{''.ljust(max_len,'═')}╝")				# draw bottom bar
 		
 	return get_integer(f"Type the number of your choice (1 to {len(menu_list)})", 1, len(menu_list), rows) - 1 # -1 to return correct list index
 
 def error_message(row:int, error_type:str, user_input:str, min_value:float = 0, max_value:float = 0) -> None:
-	''' display error message. If row number supplied overwrite with delay '''
+	''' private method. display error message. If row number supplied overwrite with delay '''
 	
 	message = "Just pressing the Enter key or spacebar doesn't work" # default for "noinput"
 	
@@ -151,15 +176,17 @@ def error_message(row:int, error_type:str, user_input:str, min_value:float = 0, 
 		clear_input_field(row)
 
 def set_cursor_pos(row:int, col:int) -> None:
-	''' Sets the cursor position '''
+	''' public method Sets the cursor position '''
 	print(f"\033[{row};{col}H", end = '')
 	
 def clear_input_field(row:int) -> None:
+	''' use set_cursor_pos to delete a line of text '''
 	if row >= 0:
 		set_cursor_pos(row, 0)
 		print(blank)
 		set_cursor_pos(row, 0)
 		
 def sleep(s:float) -> None:
+	''' public method '''
 	time.sleep(s)
 	
